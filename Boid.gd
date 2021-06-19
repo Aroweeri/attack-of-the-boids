4a803bee48030bd32c1199b6c722b17c45f54d6b
Dylan Sutton <dylanlsutton@gmail.com>
2021-06-19 19:45:56 -0400

    Add basic level and boids.

---
+++ Boid.gd
@@ -0,0 +1,81 @@
+extends KinematicBody2D
+
+var cohesionForce = 0.5;
+var alignForce = 0.05;
+var separationForce = 2.8;
+var avoidCollisionForce = 380;
+var speed = 250
+
+var velocity;
+var rng = RandomNumberGenerator.new();
+
+func _ready():
+	rng.randomize();
+	velocity = Vector2(rng.randf_range(-1,1)*speed, rng.randf_range(-1,1)*speed);
+
+func cohesion(delta):
+	var bodies = $Area2D.get_overlapping_bodies();
+	if(bodies.size() < 2):
+		return;
+	velocity = velocity.normalized();
+	var centroid = Vector2.ZERO;
+	for body in bodies:
+		if(body == self):
+			continue;
+		centroid += body.position;
+	centroid /= bodies.size()-1;
+	velocity += (centroid - position) * cohesionForce * delta;
+	velocity = velocity.normalized();
+	velocity *= speed;
+	
+func separation(delta):
+	var bodies = $Area2D.get_overlapping_bodies();
+	if(bodies.size() < 2):
+		return;
+	velocity = velocity.normalized();
+	var desired = Vector2.ZERO
+	for body in bodies:
+		if(body == self):
+			continue;
+		desired += (position - body.position) / position.distance_to(body.position);
+	velocity += desired * delta * separationForce;
+	velocity = velocity.normalized();
+	velocity *= speed;
+
+func avoid_collision(delta):
+	var distanceToCollision;
+	velocity = velocity.normalized();
+	if ($RayCastStarboard.is_colliding()):
+		distanceToCollision = position.distance_to($RayCastStarboard.get_collision_point());
+		velocity += velocity.rotated($RayCastStarboard.rotation) * delta * avoidCollisionForce * 1/distanceToCollision ;
+	elif ($RayCastPort.is_colliding()):
+		distanceToCollision = position.distance_to($RayCastPort.get_collision_point());
+		velocity += velocity.rotated(-$RayCastPort.rotation) * delta * avoidCollisionForce * 1/distanceToCollision;
+	velocity = velocity.normalized();
+	velocity *= speed;
+		
+func align(delta):
+	var bodies = $Area2D.get_overlapping_bodies();
+	if(bodies.size() < 2):
+		return
+	velocity = velocity.normalized();
+	var avg = Vector2.ZERO;
+	for body in bodies:
+		if(body == self):
+			continue;
+		avg += body.velocity;
+	avg /= bodies.size();
+	velocity += (avg - velocity) * alignForce * delta;
+	velocity = velocity.normalized();
+	velocity *= speed;
+	
+func _process(delta):
+	rotation = velocity.angle()
+	pass
+	
+func _physics_process(delta):
+	cohesion(delta);
+	separation(delta);
+	align(delta);
+	avoid_collision(delta);
+	move_and_slide(velocity)
