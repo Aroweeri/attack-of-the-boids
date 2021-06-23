d4bf0904854f26dd63bdb78cd23ec0fb1dc80b66
Dylan Sutton <dylanlsutton@gmail.com>
2021-06-22 22:09:34 -0400

    Player has hiding spots where boids wont attack them

--- scripts/Level1.gd
+++ scripts/Level1.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 onready var boidScene = preload("res://scenes/Boid.tscn");
-var numBoids = 10;
+var numBoids = 100;
 var rng = RandomNumberGenerator.new();
 var boids = [];
 
@@ -39,34 +39,45 @@ func _ready():
 func _on_cohesion_value_changed(value):
 	for boid in self.boids:
 		boid.cohesionForce = value;
-	get_node("GridContainer/cohesion_label").text = "cohesion " +str(value);
+	get_node("CanvasLayer/GridContainer/cohesion_label").text = "cohesion " +str(value);
 
 
 func _on_align_value_changed(value):
 	for boid in self.boids:
 		boid.alignForce = value;
-	get_node("GridContainer/align_label").text = "align " +str(value);
+	get_node("CanvasLayer/GridContainer/align_label").text = "align " +str(value);
 
 
 func _on_separation_value_changed(value):
 	for boid in self.boids:
 		boid.separationForce = value;
-	get_node("GridContainer/separation_label").text = "separation " +str(value);
+	get_node("CanvasLayer/GridContainer/separation_label").text = "separation " +str(value);
 
 
 func _on_view_value_changed(value):
 	for boid in self.boids:
 		boid.get_node("Area2D/CollisionShape2D").shape.radius = value;
-	get_node("GridContainer/view_label").text = "view " +str(value);
+	get_node("CanvasLayer/GridContainer/view_label").text = "view " +str(value);
 
 
 func _on_obstacle_value_changed(value):
 	for boid in self.boids:
 		boid.avoidCollisionForce = value;
-	get_node("GridContainer/obstacle_label").text = "obstacle " +str(value);
+	get_node("CanvasLayer/GridContainer/obstacle_label").text = "obstacle " +str(value);
 
 
 func _on_speed_value_changed(value):
 	for boid in self.boids:
 		boid.speed = value;
-	get_node("GridContainer/speed_label").text = "speed " + str(value);
+	get_node("CanvasLayer/GridContainer/speed_label").text = "speed " + str(value);
+
+
+func _on_Area2D_body_entered(body):
+	$CanvasLayer/GridContainer/hidden.text = "HIDDEN";
+	if(body == $Player):
+		$Player.set_hidden(true);
+		
+func _on_Area2D_body_exited(body):
+	$CanvasLayer/GridContainer/hidden.text = "EXPOSED";
+	if(body == $Player):
+		$Player.set_hidden(false);
