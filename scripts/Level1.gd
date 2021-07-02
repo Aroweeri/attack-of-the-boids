d60fd4c07bcd1674eb46f48dcbf8b263d314a27c
Dylan Sutton <dylanlsutton@gmail.com>
2021-07-01 20:37:45 -0400

    Update level layout, boids behave better in level.

--- scripts/Level1.gd
+++ scripts/Level1.gd
@@ -37,37 +37,37 @@ func _ready():
 
 
 func _on_cohesion_value_changed(value):
-	for boid in self.boids:
+	for boid in get_tree().get_nodes_in_group("boids"):
 		boid.cohesionForce = value;
 	get_node("CanvasLayer/GridContainer/cohesion_label").text = "cohesion " +str(value);
 
 
 func _on_align_value_changed(value):
-	for boid in self.boids:
+	for boid in get_tree().get_nodes_in_group("boids"):
 		boid.alignForce = value;
 	get_node("CanvasLayer/GridContainer/align_label").text = "align " +str(value);
 
 
 func _on_separation_value_changed(value):
-	for boid in self.boids:
+	for boid in get_tree().get_nodes_in_group("boids"):
 		boid.separationForce = value;
 	get_node("CanvasLayer/GridContainer/separation_label").text = "separation " +str(value);
 
 
 func _on_view_value_changed(value):
-	for boid in self.boids:
+	for boid in get_tree().get_nodes_in_group("boids"):
 		boid.get_node("Area2D/CollisionShape2D").shape.radius = value;
 	get_node("CanvasLayer/GridContainer/view_label").text = "view " +str(value);
 
 
 func _on_obstacle_value_changed(value):
-	for boid in self.boids:
+	for boid in get_tree().get_nodes_in_group("boids"):
 		boid.avoidCollisionForce = value;
 	get_node("CanvasLayer/GridContainer/obstacle_label").text = "obstacle " +str(value);
 
 
 func _on_speed_value_changed(value):
-	for boid in self.boids:
+	for boid in get_tree().get_nodes_in_group("boids"):
 		boid.speed = value;
 	get_node("CanvasLayer/GridContainer/speed_label").text = "speed " + str(value);
 
