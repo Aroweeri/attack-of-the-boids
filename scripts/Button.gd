ab35fff3c120722fb203667fef11717146779bd2
Aroweeri <dylanlsutton@gmail.com>
2021-11-21 19:02:05 -0500

    Button makes a click sound when you touch it.

--- scripts/Button.gd
+++ scripts/Button.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 func _on_button_body_entered(body):
 	if(body.is_in_group("players")):
+		$ClickPressSound.play();
 		$Sprite.modulate = Color(0.2,0.2,0.2);
 
 func _on_button_body_exited(body):
 	if(body.is_in_group("players")):
+		$ClickReleaseSound.play();
 		$Sprite.modulate = Color(1,1,1);
